c72eaef95d9a24033f51ad17e22a502c5c7dcb07
Akaki Zghuladze <zghuladze16@gmail.com>
2025-01-05 21:09:28 +0400

    initial client side code

---
+++ client/app/welcome/welcome.tsx
@@ -0,0 +1,10 @@
+export function Welcome() {
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <header className="flex flex-col items-center gap-12">
+          <h1 className="text-6xl font-bold text-indigo-300">Gurian Belote</h1>
+          <h1 className="text-3xl font-bold white">Coming Soon...</h1>
+        </header>
+    </main>
+  );
+}
