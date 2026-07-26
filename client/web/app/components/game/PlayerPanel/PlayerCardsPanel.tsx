06efeb43ea86eea63825b6b78ba765d918e74696
Giorgi Megrelidze <g.megrelidze7@gmail.com>
2026-07-26 20:43:01 +0100

    implement skipping declarations in web client

--- client/web/app/components/game/PlayerPanel/PlayerCardsPanel.tsx
+++ client/web/app/components/game/PlayerPanel/PlayerCardsPanel.tsx
@@ -1,6 +1,8 @@
+import { useState } from 'react'
 import type { Card } from '@gurian-belote/lib'
 import CardFace from '../CardFace'
 import { useGameClient } from '../GameContext'
+import SkipDeclarationsToggle from './SkipDeclarationsToggle'
 
 interface PlayerCardsPanelProps {
 	cards: Card[]
@@ -14,33 +16,42 @@ const PlayerCardsPanel = ({
 	disabled,
 }: PlayerCardsPanelProps) => {
 	const gameClient = useGameClient()
+	const [skipDeclarations, setSkipDeclarations] = useState(false)
 
 	const onCardClick = (card: Card) => {
-		gameClient.playCard(card)
+		gameClient.playCard(card, skipDeclarations)
 	}
 
 	return (
-		<div className="flex justify-center items-center -space-x-4">
-			{cards.map((card, index) => {
-				const notPlayable = !playableCards.some((playableCard) =>
-					playableCard.equals(card)
-				)
-				const cardClassName =
-					disabled || notPlayable
-						? 'brightness-70 cursor-not-allowed pointer-events-none'
-						: ''
-				return (
-					<CardFace
-						key={index}
-						card={card}
-						onClick={
-							disabled || notPlayable ? () => {} : onCardClick
-						}
-						hover
-						className={cardClassName}
-					/>
-				)
-			})}
+		<div className="flex flex-col items-center gap-3 w-full">
+			<SkipDeclarationsToggle
+				active={skipDeclarations}
+				disabled={disabled}
+				onToggle={() => setSkipDeclarations((prev) => !prev)}
+			/>
+
+			<div className="flex justify-center items-center -space-x-4">
+				{cards.map((card, index) => {
+					const notPlayable = !playableCards.some((playableCard) =>
+						playableCard.equals(card)
+					)
+					const cardClassName =
+						disabled || notPlayable
+							? 'brightness-70 cursor-not-allowed pointer-events-none'
+							: ''
+					return (
+						<CardFace
+							key={index}
+							card={card}
+							onClick={
+								disabled || notPlayable ? () => {} : onCardClick
+							}
+							hover
+							className={cardClassName}
+						/>
+					)
+				})}
+			</div>
 		</div>
 	)
 }
